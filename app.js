f5e61232d7c7cf7fb21d432d8b2481dbea3abb6c
prathm455 <prathmeshm804@gmail.com>
2025-11-29 14:15:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function w_api(){
+    let city=document.getElementById("city").value;
+    fetch(`http://api.weatherapi.com/v1/current.json?key=e66b8e9689fd47c797b60402252911&q=${city}&aqi=yes`)
+    .then(response=>response.json())
+    .then(data=>{
+       alert(`${"Current temp: ",data.current.temp_c}`);
+    })
+}
